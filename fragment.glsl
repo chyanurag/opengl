b1c2df3a535c9391642f990373a6a34eb115b396
anurag <anurag23095@gmail.com>
2024-11-18 11:34:46 +0530

    added a rotating cube

--- fragment.glsl
+++ fragment.glsl
@@ -1,16 +1,13 @@
 #version 330 core
 
 out vec4 FragColor;
-in vec3 color;
 
 in vec2 TexCoord;
 
 uniform sampler2D texture1;
-uniform sampler2D texture2;
-uniform float mixValue;
 
 void main() {
 
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue);
+    FragColor = texture(texture1, TexCoord);
 
 }
--- main.cc
+++ main.cc
@@ -1,3 +1,4 @@
+// comment
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
@@ -15,21 +16,93 @@
 #include <glm/gtc/type_ptr.hpp>
 
 float vertices[] = {
-        // positions          // colors           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 0.7f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 0.7f, 0.0f,   0.0f, 1.0f  // top left 
-    };
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
 unsigned int indices[] = {
     0, 1, 3, // first triangle
     1, 2, 3  // second triangle
 };
 
+glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
+float zTrans = 0.f;
+float xTrans = 0.f;
+
+float xRot = 0.f;
+float yRot = 0.f;
+float zRot = 0.f;
+
+void process_events(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        zTrans += .1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        zTrans -= .1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        xTrans -= .1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        xTrans += .1f;
+    }
+}
+
 int main() {
     
-    const int WIDTH = 1000;
-    const int HEIGHT = 1000;
+    const int WIDTH = 800;
+    const int HEIGHT = 800;
     Renderer renderer(WIDTH, HEIGHT, "Opengl");
     glewInit();
 
@@ -40,62 +113,42 @@ int main() {
     VBO vbo(vertices, sizeof(vertices));
     EBO ebo(indices, sizeof(indices));
 
-    vao.LinkVertexAttrib(vbo, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-    vao.LinkVertexAttrib(vbo, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3*sizeof(float)));
-    vao.LinkVertexAttrib(vbo, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    vao.LinkVertexAttrib(vbo, 0, 3, GL_FLOAT, 5 * sizeof(float), (void*)0);
+    vao.LinkVertexAttrib(vbo, 1, 2, GL_FLOAT, 5 * sizeof(float), (void*)(3*sizeof(float)));
     vao.Unbind();
     vbo.Unbind();
 
     stbi_set_flip_vertically_on_load(true);
 
-    Texture2D texture("modiji.jpg");
-    Texture2D texture2("meloni.png");
+    Texture2D texture("cube.jpg");
 
     shader.use_program();
     shader.SetInt("texture1", 0);
-    shader.SetInt("texture2", 1);
 
-    float mixValue = .1f;
-    float mixMult = 1.f;
     float angle = 0.f;
-    float angleMult = 1.f;
 
-    shader.SetFloat("mixValue", mixValue);
-
-    shader.use_program();
-
-    glm::mat4 proj = glm::perspective(glm::radians(90.f),(float)WIDTH/(float)HEIGHT, .1f, 5000.f);
-    glm::mat4 model = glm::mat4(1.f);
-    model = glm::rotate(model, glm::radians(-55.f), glm::vec3(1.f, 0.f, 0.f));
-    glm::mat4 view = glm::mat4(1.f);
-    view = glm::translate(view, glm::vec3(0.f, 0.f, -1.f));
-
+    glEnable(GL_DEPTH_TEST);
+    glm::mat4 view          = glm::mat4(1.0f);
+    glm::mat4 projection    = glm::mat4(1.0f);
+    projection = glm::perspective(glm::radians(90.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+    view       = glm::translate(view, glm::vec3(xTrans, 0.0f, -2.f));
+    glm::mat4 model = glm::mat4(1.0f);
     shader.SetMat4("model", model);
-    shader.SetMat4("projection", proj);
+    shader.SetMat4("projection", projection);
     shader.SetMat4("view", view);
 
-    glm::mat4 trans = glm::mat4(1.f);
-
     std::function<void (void)> draw = [&] (){
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glActiveTexture(GL_TEXTURE0);
         texture.Bind();
-        glActiveTexture(GL_TEXTURE1);
-        texture2.Bind();
-        shader.use_program();
-        if (mixValue > 1.f) {
-            mixMult = -1.f;
-        } else if (mixValue < 0.f) {
-            mixMult = 1.f;
-        }
-        mixValue += mixMult * .001f;
-        angle += .005f;
-        shader.SetFloat("mixValue", mixValue);
-        trans = glm::rotate(trans, glm::radians(angle), glm::vec3(0.f, 0.f, 1.f));
-        shader.SetMat4("transform", trans);
-        vao.Bind();
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
+        shader.use_program();
+        angle += .01f;
+        view = glm::rotate(view, glm::radians(angle), glm::vec3(1.f, 1.f, 1.f));
+        shader.SetMat4("view", view);
+        vao.Bind();
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        process_events(renderer.window);
     };
 
     renderer.run(draw);
